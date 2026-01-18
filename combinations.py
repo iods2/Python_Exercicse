6702f424b6953908adb15784170ebe028522a76c
iods <benhammouda.hichem@univ-ghardaia.edu.dz>
2026-01-18 21:22:54 +0100

    With fact Iterative function

--- combinations.py
+++ combinations.py
@@ -11,3 +11,17 @@ def combinations(n, k):
         result = result * (n - i) // (i + 1)
     
     return result
+
+def fact(x):
+   
+    if x < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+    
+    if x == 0 or x == 1:
+        return 1
+    
+    result = 1
+    for i in range(2, x + 1):
+        result *= i
+    
+    return result
