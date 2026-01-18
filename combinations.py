389d08ce5bba5bab1fc98d461eb0a397ca8c160b
iods <benhammouda.hichem@univ-ghardaia.edu.dz>
2026-01-18 21:28:05 +0100

    Using the math library

--- combinations.py
+++ combinations.py
@@ -1,16 +1,8 @@
+import math
+
 def combinations(n, k):
-    
-    if k > n or k < 0:
-        return 0
-    if k == 0 or k == n:
-        return 1
-    k = min(k, n - k)
-    
-    result = 1
-    for i in range(k):
-        result = result * (n - i) // (i + 1)
-    
-    return result
+
+    return math.comb(n, k)
 
 def fact(x):
     
